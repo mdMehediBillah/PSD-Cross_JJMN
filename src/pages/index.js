90203679558b918ef2962e9acbe285f5afd305c3
mdMehediBillah <mailbillah@gmail.com>
2024-07-15 11:05:09 +0200

    Routes, Navigation added to app

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,15 @@ import HomePage from "./HomePage";
 import ProductPage from "./ProductPage";
 import PromoPage from "./PromoPage";
 import SearchPage from "./SearchPage";
+import NotFounfPage from "./NotFounfPage";
 
 // export all pages
-export { AboutPage, ContactPage, HomePage, ProductPage, PromoPage, SearchPage };
+export {
+  AboutPage,
+  ContactPage,
+  HomePage,
+  ProductPage,
+  PromoPage,
+  SearchPage,
+  NotFounfPage,
+};
